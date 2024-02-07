b95079d6302b1980f26757edcf228064edccc6b7
Evandrosc <evandromdsc@gmail.com>
2024-02-07 19:50:34 -0300

    refactor: nome logo

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
     <Link href="/" className="flex items-center gap-1">
       <LogoSvg className="h-[1.125rem] w-6 fill-blue-600 lg:h-6 lg:w-7" />
       <span className="text-lg font-bold text-blue-600 lg:text-xl">
-        Alura<span className="text-gray-800">Geek</span>
+        Click<span className="text-gray-800">Shop</span>
       </span>
     </Link>
   )
