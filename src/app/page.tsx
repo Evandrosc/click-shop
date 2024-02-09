64f92e7733723e5e76d6608109ad7706457cbba7
Evandrosc <evandromdsc@gmail.com>
2024-02-09 16:37:28 -0300

    feat: provedor na página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import { ProvedorConsulta } from '@/contexts/ProvedorConsulta'
 import { Header } from '@/components/Header/Header'
+import { Produtos } from '@/components/Produtos/Produtos'
 import { Promocional } from '@/components/Promocional/Promocional'
 
 export default function Home() {
   return (
-    <>
+    <ProvedorConsulta>
       <Header />
       <main>
         <Promocional />
+        <Produtos />
       </main>
-    </>
+    </ProvedorConsulta>
   )
 }
